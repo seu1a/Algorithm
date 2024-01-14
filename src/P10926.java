be00c0b24f07d4f3313c59a792244812294ec54c
rheeseula <rheeseula@dgsw.hs.kr>
2024-01-14 09:19:08 +0900

    🍥 | P10926 해결

---
+++ src/P10926.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class P10926 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String input = scan.next();
+
+        System.out.println(input + "??!");
+    }
+}
