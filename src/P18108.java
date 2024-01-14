bb9e4c23db336c252abe3ba1d23ea1ee038cc8fd
rheeseula <rheeseula@dgsw.hs.kr>
2024-01-14 09:22:26 +0900

    🍂 | P18108 해결

---
+++ src/P18108.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class P18108 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int year = scan.nextInt();
+
+        System.out.println(year - 543);
+    }
+}
