08e39cc1d46ad405f0b3eb9800f9d03bf7f5db3a
rheeseula <rheeseula@dgsw.hs.kr>
2024-01-21 22:02:14 +0900

    🔨 | P11382 해결

--- src/P2588.java
+++ src/P2588.java
@@ -4,13 +4,10 @@ public class P2588 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        int first = scan.nextInt();
-        int second = scan.nextInt();
+        long A = scan.nextLong();
+        long B = scan.nextLong();
+        long C = scan.nextLong();
 
-        System.out.println(first * (second % 10));
-        System.out.println(first * (second % 100 / 10));
-        System.out.println(first * (second / 100));
-
-        System.out.println(first * second);
+        System.out.println(A + B + C);
     }
 }
