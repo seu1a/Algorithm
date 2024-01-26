45137f6fc3a9c00b7edc40986a79a47a9d15c008
rheeseula <rheeseula@dgsw.hs.kr>
2024-01-26 18:37:48 +0900

    ✨ | P10950 해결

---
+++ src/P10950.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class P10950 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int T = scan.nextInt();
+
+        for (int i = 0; i < T; i++) {
+            System.out.println(scan.nextInt() + scan.nextInt());;
+        }
+    }
+}
