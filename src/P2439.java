847965934ee738532eb1763b55a3dc8552009c0a
rheeseula <rheeseula@dgsw.hs.kr>
2024-01-29 08:41:09 +0900

    🙃 | P2439 해결

---
+++ src/P2439.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+public class P2439 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= N; i++) {
+            bw.write(" ".repeat(N - i));
+            bw.write("*".repeat(i) + "\n");
+        }
+
+        bw.close();
+    }
+}
