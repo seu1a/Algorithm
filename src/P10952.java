81e438f0b92016768584b9bf4895fa2a37b18cf5
rheeseula <rheeseula@dgsw.hs.kr>
2024-01-30 09:08:01 +0900

    😳 | P10952 해결

---
+++ src/P10952.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class P10952 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        while (true) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+
+            if (A == 0 & B == 0) {
+                break;
+            }
+
+            bw.write((A + B) + "\n");
+        }
+
+        bw.close();
+    }
+}
