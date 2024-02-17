602c5b6d431df0160cc7e2dd96cb0f5c335c52b5
rheeseula <rheeseula@dgsw.hs.kr>
2024-02-17 19:13:43 +0900

    🛥 | P11654 해결

---
+++ src/P11654.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class P11654 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String chr = scanner.next();
+
+        System.out.println((int) chr.charAt(0));
+    }
+}
