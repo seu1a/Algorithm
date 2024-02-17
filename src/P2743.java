d0377e1d67b9173979931d400fe8c3b961d25f1c
rheeseula <rheeseula@dgsw.hs.kr>
2024-02-17 19:03:43 +0900

    🔉 | P2743 해결

---
+++ src/P2743.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class P2743 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String text = scanner.next();
+
+        System.out.println(text.length());
+    }
+
+}
