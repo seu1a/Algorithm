c6ad05536e6e45dda15473b133bed14f4615b023
I'm not Seula <rheeseula@gmail.com>
2024-02-27 13:10:57 +0900

    🍨 | P1152 해결

---
+++ src/P1152.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class P1152 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        StringTokenizer st = new StringTokenizer(scan.nextLine());
+
+        System.out.println(st.countTokens());
+    }
+}
