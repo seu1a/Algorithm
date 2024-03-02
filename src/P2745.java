be53fcc4e8d2c5f7b38c377156b7fa964596f366
I'm not Seula <rheeseula@gmail.com>
2024-03-03 08:49:50 +0900

    🗳 | P2745 해결

---
+++ src/P2745.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class P2745 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String B = scan.next();
+        int N = scan.nextInt();
+
+        System.out.println(Integer.parseInt(B, N));
+    }
+}
